38891d361976431f093cc7c6fdf3b416fa597067
Diogo Almeida <diogoalmeida.vlc@gmail.com>
2023-02-14 14:31:38 +0000

    created MyTriangleSmall

--- tp1/MyScene.js
+++ tp1/MyScene.js
@@ -2,6 +2,7 @@ import { CGFscene, CGFcamera, CGFaxis } from "../lib/CGF.js";
 import { MyDiamond } from "./MyDiamond.js";
 import { MyTriangle } from "./MyTriangle.js";
 import { MyParallelogram } from "./MyParallelogram.js";
+import { MyTriangleSmall } from "./MyTriangleSmall.js";
 
 /**
  * MyScene
@@ -30,6 +31,7 @@ export class MyScene extends CGFscene {
     this.diamond = new MyDiamond(this);
     this.triangle = new MyTriangle(this);
     this.parallelogram = new MyParallelogram(this);
+    this.triangleSmall = new MyTriangleSmall(this);
 
     //Objects connected to MyInterface
     this.displayAxis = true;
@@ -101,6 +103,7 @@ export class MyScene extends CGFscene {
     if(this.displayDiamond) this.diamond.display();
     if(this.displayTriangle) this.triangle.display();
     if(this.displayParallelogram) this.parallelogram.display();
+    this.triangleSmall.display();
     // ---- END Primitive drawing section
   }
 }
