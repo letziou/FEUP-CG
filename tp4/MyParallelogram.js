55635f5e881652d9e2ae13e9e1cda8d654c284de
Rafael <up201506449@fc.up.pt>
2023-03-21 12:02:02 +0000

    texture to myDiamond

---
+++ tp4/MyParallelogram.js
@@ -0,0 +1,64 @@
+import {CGFobject} from '../lib/CGF.js';
+/**
+ * MyParallelogram
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+export class MyParallelogram extends CGFobject {
+	constructor(scene) {
+		super(scene);
+		this.initBuffers();
+	}
+	
+	initBuffers() {
+		this.vertices = [
+			//first loop
+			0, 0, 0,	//0
+			1, 0, 0,	//1
+			1, 1, 0,	//2
+      2, 0, 0,  //3 
+      2, 1, 0,  //4
+      3, 1, 0,  //5
+			//second loop
+			0, 0, 0,	//0
+			1, 0, 0,	//1
+			1, 1, 0,	//2
+      2, 0, 0,  //3 
+      2, 1, 0,  //4
+      3, 1, 0,  //5
+		];
+
+		this.indices = [
+			0, 1, 2,
+			2, 1, 3,
+			2, 3, 4,
+			4, 3, 5, 
+			5, 3, 4,
+			4, 3, 2,
+			3, 1, 2,
+			2, 1, 0
+		];
+
+		this.normals = [
+			0, 0, 1,
+			0, 0, 1,
+			0, 0, 1,
+			0, 0, 1,
+			0, 0, 1,
+			0, 0, 1,
+			0, 0, -1,
+			0, 0, -1,
+			0, 0, -1,
+			0, 0, -1,
+			0, 0, -1,
+			0, 0, -1
+		];
+
+		//The defined indices (and corresponding vertices)
+		//will be read in groups of three to draw triangles
+		this.primitiveType = this.scene.gl.TRIANGLES;
+
+		this.initGLBuffers();
+	}
+}
+
