59d331edd5c12f2be8ec25377aa886de2dae2368
Rafael <up201506449@fc.up.pt>
2023-04-20 00:46:52 +0100

    bird done

--- project/MyBirdTail.js
+++ project/MyBirdTail.js
@@ -1,10 +1,10 @@
 import {CGFobject} from '../lib/CGF.js';
 /**
- * MyParallelogram
+ * MyTriangleSmall
  * @constructor
  * @param scene - Reference to MyScene object
  */
-export class MyBirdWing extends CGFobject {
+export class MyBirdTail extends CGFobject {
 	constructor(scene) {
 		super(scene);
 		this.initBuffers();
@@ -12,22 +12,20 @@ export class MyBirdWing extends CGFobject {
 	
 	initBuffers() {
 		this.vertices = [
-			0, 0, 0,	//0
-			1, 0, 0,	//1
-			1, 1, 0,	//2
-      2, 0, 0,  //3 
-      2, 1, 0,  //4
-      3, 1, 0,  //5
+			1, 0, 0,	//0
+			0, 1, 0,	//1
+			-1, 0, 0,	//2
 		];
 
 		//Counter-clockwise reference of vertices
 		this.indices = [
-			0, 1, 2, 3, 4, 3, 2, 1, 0,
+			0, 1, 2,
+      2, 1, 0,
 		];
 
 		//The defined indices (and corresponding vertices)
 		//will be read in groups of three to draw triangles
-		this.primitiveType = this.scene.gl.TRIANGLE_STRIP;
+		this.primitiveType = this.scene.gl.TRIANGLES;
 
 		this.initGLBuffers();
 	}
